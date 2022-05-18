b7a36c98af5cfa379a8db088f49e58e1721d79b1
Nawaf-Almansour <nawaaf.almansour@gmail.com>
2022-05-18 22:16:43 +0300

    updata postlist

--- src/components/postlist.js
+++ src/components/postlist.js
@@ -22,7 +22,7 @@ const PostList = () => {
                             </div>
                             <small>Published on {post.date} by {post.author}</small>
                             <hr/>
-                            <Markdown source={excerptList[i]} escapeHtml={false}  children={}/>
+                            <Markdown source={excerptList[i]} escapeHtml={false}/>
                             <small><Link className="links" to={`/post/${post.id}`}>Read more</Link></small>
                         </div>
                     )
