b2a8a4eae9ceda03d76424bdf869c229c6b120c1
Nawaf-Almansour <nawaaf.almansour@gmail.com>
2022-05-18 22:54:00 +0300

    add  router index

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route} from "react-router-dom"
 import './index.css';
+import "typeface-raleway"
 import reportWebVitals from './reportWebVitals';
 import Home from "./pages/home";
+import About from "./pages/about";
+import NotFound from "./pages/notfound";
+import Post from "./pages/post";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
       <Router>
       <div>
           <Route exact path="/" component={Home} />
-          {/*<Route exact path="/about" component={About} />*/}
-          {/*<Route exact path="/404" component={NotFound} />*/}
-          {/*<Route exact path="/post/:id" render={props => <Post {...props} />} />*/}
+          <Route exact path="/about" component={About} />
+          <Route exact path="/404" component={NotFound} />
+          <Route exact path="/post/:id" render={props => <Post {...props} />} />
       </div>
       </Router>,
-  </React.StrictMode>
+    document.getElementById('root')
+
 );
 
 // If you want to start measuring performance in your app, pass a function
