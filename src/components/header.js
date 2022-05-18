0c6be19c136e61be4bd5de781057c335ce361b8e
Nawaf-Almansour <nawaaf.almansour@gmail.com>
2022-05-19 00:25:05 +0300

    fix add

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ import "./components.css"
 const Header = () => {
     return (
         <div className="header">
-            <h1>React Markdown Blog</h1>
+            <h1>simple CMS App</h1>
         </div>
     )
 }
